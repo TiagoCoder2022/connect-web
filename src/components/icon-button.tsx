34694d65ffdbe3eb205de981c7e9d7dc46e7221a
TiagoCoder2022 <tiagonewalves@hotmail.com>
2025-02-19 20:27:52 -0300

    refactor: IconButton

--- src/components/icon-button.tsx
+++ src/components/icon-button.tsx
@@ -1,15 +1,16 @@
-import { ReactNode} from "react"
+import type { ComponentProps } from 'react'
+import { twMerge } from 'tailwind-merge'
 
-interface IconButtonProps {
-    children?: ReactNode;
-}
+interface ButtonProps extends ComponentProps<'button'> {}
 
-const IconButton = ({children}: IconButtonProps) => {
-    return (
-        <button className="p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900">
-            {children}
-        </button>
-    )
+export function IconButton({ className, ...props }: ButtonProps) {
+  return (
+    <button
+      className={twMerge(
+        'p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900',
+        className
+      )}
+      {...props}
+    />
+  )
 }
- 
-export default IconButton;
